f80d863edb6472d7eaf3fe3141ac161b554bf0b1
LinJing-Liu <linjingl93@gmail.com>
2022-06-16 11:02:14 -0400

    add redeploy script

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	$('.header').height($(window).height() * 0.8);
+	$('.header').height($(window).height());
 });
 // setTimeout(function() {
 // 	$('#demo-modal').modal();
