d14e347c06730f2d66e7f0cc31e285f6556734da
LinJing-Liu <linjingl93@gmail.com>
2022-06-22 17:25:21 -0400

    add timeline post page

--- app/__init__.py
+++ app/__init__.py
@@ -43,7 +43,7 @@ def portfolio():
 
 @app.route('/timeline')
 def timeline():
-    return render_template('timeline.html', title="Timeline")
+    return render_template('timeline.html', title="Timeline", url=os.getenv("URL"))
 
 @app.route('/api/timeline_post', methods=['POST'])
 def post_time_line_post():
