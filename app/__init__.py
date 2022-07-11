e2fec882d732289dd86f1ff3727189e19a939745
LinJing-Liu <linjingl93@gmail.com>
2022-07-11 12:13:48 -0400

    fix test issues

--- app/__init__.py
+++ app/__init__.py
@@ -11,10 +11,11 @@ load_dotenv()
 app = Flask(__name__)
 
 if os.getenv("TESTING") == "true":
-	print("Runnign in test mode")
+	print("Running in test mode")
 	mydb = SqliteDatabase('file:memory?mode=memory&cache=shared', uri=True)
 else:
-	mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"),
+    print("Running in standard mode")
+    mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"),
     		user=os.getenv("MYSQL_USER"),
     		password=os.getenv("MYSQL_PASSWORD"),
     		host=os.getenv("MYSQL_HOST"),
--- setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-python -m venv python3-virtualenv
-source python3-virtualenv/bin/activate
-export FLASK_ENV=development
-flask run
